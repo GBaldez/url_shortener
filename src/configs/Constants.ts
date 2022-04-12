52bb22823cca5c2412e778ea0e74f866f3d9397c
GBaldez <83733139+GBaldez@users.noreply.github.com>
2022-04-11 22:42:47 -0300

    check url is valid

--- src/configs/Constants.ts
+++ src/configs/Constants.ts
@@ -2,5 +2,5 @@ export const config = {
 	API_URL: 'http://localhost:5000',
 	
 	MONGO_CONNECTION:
-	'mongodb+srv://<user>:<password>@new-url.lhwdp.mongodb.net/<cluster-name>?retryWrites=true&w=majority',
+	'write your mongo connection string here',
 }
--- src/controller/URLController.ts
+++ src/controller/URLController.ts
@@ -2,19 +2,31 @@ import { Request, Response } from 'express';
 import shortId from 'shortid';
 import { config } from '../configs/Constants';
 import { URLModel } from '../database/model/URL';
+const validUrl = require('valid-url');
 
 export class URLController {
     public async shorten (req: Request, response: Response): Promise<void> {
         const { originURL } = req.body;
-        const url = await URLModel.findOne({ originURL })
-        if (url) {
-            response.json(url)
-            return
+
+        if(validUrl.isUri(originURL)) {
+            try {
+                const url = await URLModel.findOne({ originURL })
+
+                if (url) {
+                    response.json(url)
+                    return
+                } else {
+                    const hash = shortId.generate()
+                    const shortURL = `${config.API_URL}/${hash}`
+                    const newURL = await URLModel.create({ hash, shortURL, originURL })
+                    response.json(newURL)
+                }
+            } catch (err) {
+                response.status(500).json({ message: err.message })
+            }	
+        } else {
+            response.status(400).json({ message: 'Invalid URL' })
         }
-        const hash = shortId.generate()
-        const shortURL = `${config.API_URL}/${hash}`
-        const newURL = await URLModel.create({ hash, shortURL, originURL })
-        response.json(newURL)
     }
 
     public async redirect(req: Request, response: Response): Promise<void> {
